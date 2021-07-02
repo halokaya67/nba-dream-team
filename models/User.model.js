63e3250d1220fd7b7c9bf396c1603fdcdc9d08ae
Halil Ibrahim Kaya <halilibrahim280@gmail.com>
2021-07-02 12:35:25 +0200

    merge test

--- models/User.model.js
+++ models/User.model.js
@@ -4,9 +4,11 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
+    required: true,
+    unique: true
   },
   password: String,
+  required: true
 });
 
 const User = model("User", userSchema);
---
+++ routes/auth/auth.routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const UserModel = require('../models/User.model');
+const bcrypt = require('bcrypt');
+
+
+
+module.exports = router;
